fe2c02ebbac4b59dc1e01ea1c7f9ff2c3757f684
Trần Quốc Trường <82871463+truong-tran-quoc@users.noreply.github.com>
2022-08-02 14:23:33 +0700

    Update h5peditor.js

--- h5peditor.js
+++ h5peditor.js
@@ -85,7 +85,7 @@ ns.loadJs = function (src, done) {
   }
 }
 getAjaxUrl = function (action, parameters) {
-        var url = "api/Samples/Newbie/TruongTQ/H5P/" + action;
+        var url = "/api/Samples/Newbie/TruongTQ/H5P/" + action;
         if (parameters !== undefined) {
             for (var key in parameters) {
                 url += (url.indexOf('?') === -1 ? '?' : '&') + key + '=' + parameters[key];
